435467ae089b55d649fd977d2b17354938f4a34e
Sérgio Luiz Moral Marques <sergio@lapa01.academico.cotuca.unicamp.br>
2022-05-31 18:21:05 -0300

    usando package.jsons

--- base.js
+++ base.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
-http.createServer((req,res)=>{
+base = http.createServer((req,res)=>{
     res.writeHead(200,{'Content-type': 'text/html; charset=utf-8'});
     res.end('<html><head></head><body><h1>Alo turma<br>tudo bem<br></h1></body></html>');
-}).listen(3001);
-
+});
+base.listen(process.env.PORT || 8080 )
 console.log('Control C para parar...')
