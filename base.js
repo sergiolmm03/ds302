e2739060235a7d980a4cc1f9717aa742d840c672
Sérgio Luiz Moral Marques <sergio@lapa01.academico.cotuca.unicamp.br>
2022-06-14 11:44:47 -0300

    var ambiente

--- base.js
+++ base.js
@@ -1,4 +1,34 @@
 const http = require('http');
+/*
+Host
+ec2-3-234-131-8.compute-1.amazonaws.com
+Database
+d7i5d877obafpq
+User
+tckffnnvzulblb
+Port
+5432
+Password
+3818ceea34b6f95eb8b15968771643ffab34df2db16a3208763a8a6272f97288
+URI
+postgres://tckffnnvzulblb:3818ceea34b6f95eb8b15968771643ffab34df2db16a3208763a8a6272f97288@ec2-3-234-131-8.compute-1.amazonaws.com:5432/d7i5d877obafpq
+Heroku CLI
+heroku pg:psql postgresql-solid-42349 --app ds302
+
+*/ 
+const { Client } = require('pg')
+const client = new Client({
+  user: 'tckffnnvzulblb',
+  host: 'ec2-3-234-131-8.compute-1.amazonaws.com',
+  database: 'd7i5d877obafpq',
+  password: '3818ceea34b6f95eb8b15968771643ffab34df2db16a3208763a8a6272f97288',
+  port: 5432,
+})
+client.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
 
 base = http.createServer((req,res)=>{
     res.writeHead(200,{'Content-type': 'text/html; charset=utf-8'});
